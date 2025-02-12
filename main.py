397b7f1688ba51edd50be8dcdf3c02a83d933098
kpookgwadi023b <kpookgwadi023b@student.wethinkcode.co.za>
2025-02-12 17:32:51 +0200

    Reorganised code

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""Welcome to Cipher Encryption Program
+    @Author: Karabo Pookgwadi
+    @Date: 12 Feb 2025
+"""
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
